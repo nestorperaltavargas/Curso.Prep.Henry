07de43a5dce95731316ae3e213a59893ee526049
FreelancerXYZ <nestorperaltavargas@gmail.com>
2022-04-15 15:11:34 +0200

    Homework JS1 corregido

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -41,7 +41,8 @@ function suma(x, y) {
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return x- y;
+  return x - y;
+}
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
